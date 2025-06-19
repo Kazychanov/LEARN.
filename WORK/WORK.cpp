04756ac52b1c4c6a15402d53b9a9b1941f7eb019
TR14WR Terriuawara <Terriuawara@gmail.com>
2025-06-19 19:12:35 +0500

    static

--- WORK/WORK.cpp
+++ WORK/WORK.cpp
@@ -16,10 +16,11 @@ private:
 
 class MyClass
 {
+	friend FriendClass;
 public:
 	MyClass(int aa, int bb);
-	friend void FriendClass::takeMyClass(MyClass& etc);
 private:
+	static int count;
 	int a;
 	int b;
 };
@@ -27,14 +28,16 @@ MyClass::MyClass(int aa, int bb)
 {
 	this->a = aa;
 	this->b = bb;
+	count++;
 }
-
+int MyClass::count = 0;
 
 
 void FriendClass::takeMyClass(MyClass& classes)
 {
 	cout << "a: " << classes.a << endl;
 	cout << "b: " << classes.b << endl;
+	cout << "c: " << classes.count << endl;
 
 }
 int main() {
@@ -43,5 +46,10 @@ int main() {
 	MyClass cs(5, 6);
 	FriendClass fc;
 	fc.takeMyClass(cs);
+
+	cout << "___________________________" << endl;
+
+	MyClass cs1(5, 6);
+	fc.takeMyClass(cs1);
 }
 
