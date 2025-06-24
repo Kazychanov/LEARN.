f51dbb038ca85e67f6a96d23248a49b1d49ac4f1
TR14WR Terriuawara <Terriuawara@gmail.com>
2025-06-24 20:43:53 +0500

    виртуальный деструктор

--- WORK/WORK.cpp
+++ WORK/WORK.cpp
@@ -2,54 +2,31 @@
 #include <string>
 using namespace std;
 
-class Weapon {
-public:
-	virtual void Shoot() = 0;
-};
 
-class Gun :public Weapon {
-public:
-	void Shoot() override {
-		cout << "BANG!" << endl;
+class A {
+public :
+	A() {
+		cout << "Выделена динамическая память под класс А" << endl;
+	}
+	virtual ~A() {
+		cout << "Очищена динамическая память под класс А" << endl;
 	}
 };
 
-class SubmachineGun : public Gun{
+class B :public A{
 public:
-	void Shoot() override 
-	{
-		cout << "BANG! BANG! BANG!" << endl;
+	B() {
+		cout << "Выделена динамическая память под класс B" << endl;
 	}
-};
-
-class Bazooka : public Weapon {
-public:
-	void Shoot() override
-	{
-		cout << "BADUUM!" << endl;
-	}
-};
-
-class Knife : public Weapon{
-public:
-	void Shoot() override
-	{
-		cout << "FHJIUTSG!" << endl;
-	}
-};
-
-class Player {
-public:
-	void Shoot(Weapon * Weapon) {
-		Weapon->Shoot();
+	~B() override{
+		cout << "Очищена динамическая память под класс B" << endl;
 	}
 };
 
 int main() {
 	setlocale(0, "");
-	Knife knife;
 
-	Player player;
-	player.Shoot(&knife);
+	A* bptr = new B;
+	delete bptr;
 }
 
