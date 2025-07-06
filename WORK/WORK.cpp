2d826e32f2afe00d8eeeaf69942e48835ce66de1
TR14WR Terriuawara <Terriuawara@gmail.com>
2025-07-06 17:16:18 +0500

    бля интерфейсы ну нахуя они нужны

--- WORK/WORK.cpp
+++ WORK/WORK.cpp
@@ -2,65 +2,35 @@
 #include <string>
 using namespace std;
 
-class Car {
-public:
-	int n = 5;
-	Car()
-	{
-		cout << "Вызвался конструктор класса Car" << endl;
-	}
-	~Car() {
-		cout << "Вызвался деструктор класса Car" << endl;
-	}
-	int Use(int n) {
-		this->n += n;
-		cout << "\nЯ еду\n" << endl;
-		return this->n;
-	}
-private:
+struct IBicycle
+{
+	void virtual TwistWheel() = 0;
+	void virtual Ride() = 0;
 };
 
-class Airplane {
-public:
-	int m = 5;
-	Airplane()
-	{
-		cout << "Вызвался конструктор класса Airplane" << endl;
+struct SimpleBicycle : public IBicycle
+{
+	void TwistWheel() override {
+		cout << " Метод TwistWheel\tкласса SimpleBicycle " << endl;
 	}
-	~Airplane()
-	{
-		cout << "Вызвался деструктор класса Airplane" << endl;
+	void Ride() override {
+		cout << " Метод Ride\tкласса SimpleBicycle " << endl;
 	}
-	void Use() {
-		cout << "\nЯ лечу\n" << endl;
-	}
-private:
 };
 
-class FlyingCar : public Car, public Airplane {
-	public:
-	FlyingCar()
-	{
-		cout << "Вызвался конструктор класса FlyingCar" << endl;
-	}
-	~FlyingCar()
-	{
-		cout << "Вызвался деcтруктор класса FlyingCar" << endl;
+struct Human
+{
+	void RideOn(IBicycle & bicycle) {
+		cout << "Крутим руль." << endl;
+		bicycle.TwistWheel();
+		cout << "Поехали " << endl;
+		bicycle.Ride();
 	}
 };
 
 int main() {
 	setlocale(0, "");
-
-	FlyingCar fc;
-
-	Car* car_ptr = &fc;
-	car_ptr->Use(10);
-	cout << "\tn = " << car_ptr->n << endl;
-
-	Airplane* airplane_ptr = &fc;
-	airplane_ptr->Use();
-	cout << "\tm = " << airplane_ptr->m << endl;
-
-	cout << "\tn = " << car_ptr->n << endl;
+	Human h;
+	SimpleBicycle sb;
+	h.RideOn(sb);
 }
