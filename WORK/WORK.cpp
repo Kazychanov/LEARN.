c5da18a710ed0e1dc958ce1502084ac297c43605
TR14WR Terriuawara <Terriuawara@gmail.com>
2025-07-06 15:17:13 +0500

    множественное наследование одинаковых функций

--- WORK/WORK.cpp
+++ WORK/WORK.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 class Car {
 public:
+	int n = 5;
 	Car()
 	{
 		cout << "Вызвался конструктор класса Car" << endl;
@@ -11,14 +12,17 @@ public:
 	~Car() {
 		cout << "Вызвался деструктор класса Car" << endl;
 	}
-	void Drive() {
-		cout << "Я еду" << endl;
+	int Use(int n) {
+		this->n += n;
+		cout << "\nЯ еду\n" << endl;
+		return this->n;
 	}
 private:
 };
 
 class Airplane {
 public:
+	int m = 5;
 	Airplane()
 	{
 		cout << "Вызвался конструктор класса Airplane" << endl;
@@ -27,8 +31,8 @@ public:
 	{
 		cout << "Вызвался деструктор класса Airplane" << endl;
 	}
-	void Fly() {
-		cout << "Я лечу" << endl;
+	void Use() {
+		cout << "\nЯ лечу\n" << endl;
 	}
 private:
 };
@@ -50,4 +54,13 @@ int main() {
 
 	FlyingCar fc;
 
+	Car* car_ptr = &fc;
+	car_ptr->Use(10);
+	cout << "\tn = " << car_ptr->n << endl;
+
+	Airplane* airplane_ptr = &fc;
+	airplane_ptr->Use();
+	cout << "\tm = " << airplane_ptr->m << endl;
+
+	cout << "\tn = " << car_ptr->n << endl;
 }
