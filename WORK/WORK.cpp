b4b6305ae434af25a358b7517cfb0c1507a1264c
TR14WR Terriuawara <Terriuawara@gmail.com>
2025-07-06 14:59:57 +0500

    конструктор и деструктор множественного наследования классов.

--- WORK/WORK.cpp
+++ WORK/WORK.cpp
@@ -2,19 +2,52 @@
 #include <string>
 using namespace std;
 
-class A {
+class Car {
 public:
-	int i = 15;
-	double y = 3.14;
+	Car()
+	{
+		cout << "Вызвался конструктор класса Car" << endl;
+	}
+	~Car() {
+		cout << "Вызвался деструктор класса Car" << endl;
+	}
+	void Drive() {
+		cout << "Я еду" << endl;
+	}
+private:
+};
+
+class Airplane {
+public:
+	Airplane()
+	{
+		cout << "Вызвался конструктор класса Airplane" << endl;
+	}
+	~Airplane()
+	{
+		cout << "Вызвался деструктор класса Airplane" << endl;
+	}
+	void Fly() {
+		cout << "Я лечу" << endl;
+	}
+private:
+};
+
+class FlyingCar : public Car, public Airplane {
+	public:
+	FlyingCar()
+	{
+		cout << "Вызвался конструктор класса FlyingCar" << endl;
+	}
+	~FlyingCar()
+	{
+		cout << "Вызвался деcтруктор класса FlyingCar" << endl;
+	}
 };
 
 int main() {
 	setlocale(0, "");
 
-	alignas(A)char buffer[sizeof(A) * 2];
-
-	A* a1 = new (buffer) A;
-
+	FlyingCar fc;
 
 }
-
