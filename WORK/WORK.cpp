5aed3ec1c7e2592f4b9574bb118c0d74b434d0be
TR14WR Terriuawara <Terriuawara@gmail.com>
2025-07-06 14:35:45 +0500

    не помню нахуя  делал буффер

--- WORK/WORK.cpp
+++ WORK/WORK.cpp
@@ -5,39 +5,16 @@ using namespace std;
 class A {
 public:
 	int i = 15;
-	A() {
-		cout << "Вызван конструктор A" << endl;
-	}
+	double y = 3.14;
 };
-
-
-void* operator new(size_t n) noexcept(false){
-	if (n == 0) {
-		n == 1;
-	}
-	while (true) {
-		void* p = malloc(n);
-		if (p) {
-			cout << "Выделено " << n << " байт." << endl;
-			return p;
-		}
-		new_handler nw = get_new_handler();
-		if (!nw) {
-			throw bad_alloc();
-		}
-		(*nw)();
-	}
-}
-
-void operator delete(void* ptr, size_t size) {
-	cout << "Очищено " << size << " байт" << endl;
-	free(ptr);
-}
 
 int main() {
 	setlocale(0, "");
 
-	A *a = new A;
-	delete a;
+	alignas(A)char buffer[sizeof(A) * 2];
+
+	A* a1 = new (buffer) A;
+
+
 }
 
