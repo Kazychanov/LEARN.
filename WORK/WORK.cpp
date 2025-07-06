b3cf81fb935588d7e2d942c9cf20a5cbfcd56f17
TR14WR Terriuawara <Terriuawara@gmail.com>
2025-07-06 20:05:30 +0500

    fstream

--- WORK/WORK.cpp
+++ WORK/WORK.cpp
@@ -1,55 +1,49 @@
 ﻿#include <iostream>
 #include <string>
 #include <fstream>
+#include <Windows.h>
 using namespace std;
 
-struct MyStruct
-{
-  MyStruct() {
-    x = y = z = 0;
-  }
-  MyStruct(int x, int y, int z) {
-    this->x = x;
-    this->y = y;
-    this->z = z;
-  }
-  void Print() {
-    cout << "x: " << x << " y: " << y << " z: " << z << endl;
-  }
-  int x, y, z;
-};
+
 
 int main() {
-  setlocale(0, "");
+  SetConsoleOutputCP(1251);
 
-  MyStruct ms2(10, 0, 0);
 
-  string pathname = "ЯНЕГЕЙ";
-  pathname += ".txt";
-  ofstream fout;
-  fout.open(pathname, ofstream::app);
 
-  if (fout.is_open())
-  {
-    fout.write((char*)&ms2, sizeof(MyStruct));
+  string path = "File.txt";
+
+  fstream fs;
+  fs.open(path, fstream::in | fstream::out | fstream::app);
+  if (!fs.is_open()) {
+    cout << "Ошибка открытия папки" << endl;
   }
   else {
-    cout << "Ошибка открытия" << endl;
-  }
-  fout.close();
-
-  ifstream fin;
-  fin.open(pathname);
-  if (fin.is_open()) {
-    MyStruct readPoint;
-
-    while (fin.read((char*)&readPoint, sizeof(MyStruct))) {
-      readPoint.Print();
+    while (true) {
+      cout << "Нажмите 1 для записи данных в файл" << endl;
+      cout << "Нажмите 2 для чтения данных файлa" << endl;
+      int value;
+      cin >> value;
+      fs.seekg(0);
+      string str;
+      switch (value)
+      {
+      case 1:
+        SetConsoleCP(1251);
+        cout << "Введите данные";
+        cin >> str;
+        fs << endl << str << endl;
+        SetConsoleCP(866);
+        break;
+      case 2:
+        while (!fs.eof()) {
+          str = "";
+          fs >> str;
+          cout << str << endl;
+        }
+      }
     }
   }
-  else
-    cout << "Ошибка открытия" << endl;
-
-  fin.close();
+  fs.close();
 }
 
