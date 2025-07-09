6e2b98ecf9aac4227a6be1b8f12f76aa24755271
TR14WR Terriuawara <Terriuawara@gmail.com>
2025-07-09 20:31:45 +0500

    throw

--- WORK/WORK.cpp
+++ WORK/WORK.cpp
@@ -4,55 +4,28 @@
 #include <fstream>
 using namespace std;
 
-struct MyStruct
-{
-  MyStruct() {
-    x = y = z = 0;
-  }
-  MyStruct(int x, int y, int z) {
-    this->x = x;
-    this->y = y;
-    this->z = z;
-  }
-  void Print() {
-    cout << "x: " << x << " y: " << y << " z: " << z << endl;
-  }
-  int x, y, z;
-};
-
-ostream& operator << (ostream& os, const MyStruct& point) {
-
-  os << point.x << " " << point.y << " " << point.z << "\n";
-  return os;
-}
-
-istream& operator >> (istream& is, MyStruct& point) {
-  is >> point.x >> point.y >> point.z;
-  return is;
+void FOO(int value) {
+	if (value < 0)
+		throw exception("Число меньше 0");
+	else if (value > 10)
+		throw exception("Число больше 10");
+	else
+  cout << "Переменная " << value << endl;
 }
 
 int main() {
   SetConsoleCP(1251);
   SetConsoleOutputCP(1251);
+	try
+	{
+		FOO(5);
+	}
+	catch (const exception & ex)
+	{
+		cout << ex.what() << endl;
+	}
 
-  string path = "test123.txt";
 
-  fstream fs;
-  fs.exceptions(fstream::badbit | fstream::failbit);
-  
-  try
-  {
-    cout << "открытие файла" << endl;
-    fs.open(path, fstream::in | fstream::out | fstream::app);
-    cout << "файл открыт" << endl;
-  }
-  catch (const fstream::failure & ex)
-  {
-    cout << ex.what() << endl;
-    cout << ex.code() << endl;
-    cout << "ошибка открытия файла" << endl;
-  }
-  fs.close();
   return 0;
 }
 
