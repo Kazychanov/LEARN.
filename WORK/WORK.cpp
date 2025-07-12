30ac3d3a3d2f6a14738bf35d6e6f2e16030dcaa9
TR14WR Terriuawara <Terriuawara@gmail.com>
2025-07-12 21:47:20 +0500

    специализация шаблона класса (ну плюс минус понятно. может воспользуюсь)

--- WORK/WORK.cpp
+++ WORK/WORK.cpp
@@ -31,13 +31,28 @@ private:
 	T2 value2;
 };
 
+template<typename Т2>
+struct MyStruct<string, Т2>
+{
+	MyStruct(string str1, Т2 str2) : value1(str1), value2(str2) {}
+		string value1;
+		Т2 value2;
+	void DataTypeSize() {
+		cout << sizeof(value1) << endl;
+		cout << sizeof(value2) << endl;
+	}
+};
+
 int main() {
+
 	Point a(5, 3, 6);
 
 
 	MyStruct<Point, int> ms1(a, 4);
 	ms1.DataTypeSize();
 
+	MyStruct<string, Point> ms2("1", a);
+	ms2.DataTypeSize();
 	return 0;
 }
 
