b6395e34109dd931e0a1b31230ff2dac79b3ceed
TR14WR Terriuawara <Terriuawara@gmail.com>
2025-07-15 06:51:20 +0500

    односвязанный список

--- WORK/WORK.cpp
+++ WORK/WORK.cpp
@@ -1,62 +1,161 @@
 ﻿#include <iostream>
-#include <Windows.h>
-#include <memory>
+#include <ctime>
 using namespace std;
 
-struct Point
-{
-	Point() {
-		x = y = z = 0;
-	}
-	Point(int x, int y, int z) {
-		this->x = x;
-		this->y = y;
-		this->z = z;
-	}
-	int x, y, z;
-};
-
-ostream& operator <<(ostream& os, const Point& p) {
-	os << p.x << " " << p.y << " " << p.z << endl;
-	return os;
-}
-
 template<class T>
-struct Sptr
+struct List
 {
-	Sptr(T *ptr) {
-		cout << "Вызвался конструктор умного указателя" << endl;
-
-		this->ptr = ptr;
-	}
-	~Sptr()
-	{
-		cout << "Вызвался деструктор умного указателя" << endl;
-		if (this->ptr == nullptr)
-		delete ptr;
-		this->ptr = nullptr;
-	}
-	T& operator *() {
-		return *ptr;
-	}
 private:
-	T *ptr;
+  template <class>
+  struct Node {
+    Node(T data = T(), Node* ptr = nullptr) {
+      this->data = data;
+      this->ptr = ptr;
+    }
+    Node* ptr;
+    T data;
+  };
+  Node<T>* Head;
+  int Size;
+public:
+  List<T>() {
+    this->Size = 0;
+    this->Head = nullptr;
+  }
+
+  ~List<T>()
+  {
+    clear();
+  }
+
+  void pushBack(T data) {
+    if (Head == nullptr) {
+      this->Head = new Node<T>(data);
+    }
+    else
+    {
+      Node<T>* current = Head;
+      while (current->ptr != nullptr) {
+        current = current->ptr;
+      }
+      current->ptr = new Node<T>(data);
+    }
+    this->Size++;
+
+  }
+
+  void printNode() {
+    Node<T>* current = Head;
+    int i = 1;
+    while (current != nullptr) {
+      cout << "Номер: " << i << endl;
+      cout << "Адрес:\t" << current << "\tЗначение:\t" << current->data << endl;
+      i++;
+      current = current->ptr;
+    }
+    cout << "\nСписок окончен.\t\t" << "\tЭлементов\t" << Size << endl;
+  }
+
+  void clear() {
+    if (Head != nullptr) {
+      Node<T>* current = Head;
+      Node<T>* nextptr = nullptr;
+      while (current->ptr != nullptr) {
+        nextptr = current->ptr;
+        delete current;
+        current = nextptr;
+      }
+    }
+    Head = nullptr;
+    Size = 0;
+  }
+
+  void insert(T data, int index) {
+    if (index >= 0 && index <= Size) {
+      if (index == 0) {
+        Head = new Node<T>(data, Head);
+      }
+      else {
+        Node<T>* current = Head;
+        for (int i = 1; i < index; i++) {
+          current = current->ptr;
+        }
+        current->ptr = new Node<T>(data, current->ptr);
+      }
+      Size++;
+    }
+    else {
+      cout << "Недопустимый индекс для вставки." << endl;
+    }
+  }
+
+  void remove(int index) {
+    if (index > 0 && index <= Size) {
+      if (index == 1) {
+        Node<T>* nextptr = Head->ptr;
+        delete Head;
+        Head = nextptr;
+      }
+      else {
+        Node<T>* current = Head;
+        for (int i = 1; i < index; i++) {
+          current = current->ptr;
+        }
+        Node<T>* nextptr = current->ptr;
+        current->ptr = nextptr->ptr;
+        delete nextptr;
+      }
+    }
+    Size--;
+    cout << "недопустимый индекс для удаления" << endl;
+  }
+
+  T& operator [](const int index) {
+    Node<T>* current = Head;
+    int counter = 0;
+    while (current != nullptr) {
+      if (counter == index)
+        return current->data;
+      current = current->ptr;
+      counter++;
+    }
+  }
 };
+
 
 int main() {
-	setlocale(0, "");
-	Point a(5,3,6);
+  setlocale(0, "");
+  srand(time(0));
 
-	int size;
-	cout << "Введите количество символов в массиве ";
-	cin >> size;
-	cout << endl;
-	shared_ptr<int[]> ptr(new int[size]);
-	for (int i = 0; i < size; i++) {
-		ptr[i] = rand() % 10;
-		cout << ptr[i] << "\t";
-	}
+  List<int> intlist;
+  intlist.pushBack(1);
+  intlist.pushBack(2);
+  intlist.pushBack(3);
+  intlist.pushBack(4);
+  intlist.printNode();
 
-	return 0;
+  cout << "--------------------------------------------------------------------" << endl;
+
+  int data, number;
+  cout << "Введите число и номер, после которого хотите создать узел: ";
+  cin >> data >> number;
+  intlist.insert(data, number);
+  intlist.printNode();
+  intlist.remove(number);
+  cout << "--------------------------------------------------------------------" << endl;
+
+  intlist.printNode();
+
+  intlist.clear();
+  //cout << "--------------------------------------------------------------------" << endl;
+  //intlist.pushBack(rand() % 10);
+  //intlist.pushBack(rand() % 10);
+  //intlist.pushBack(rand() % 10);
+  //intlist.pushBack(rand() % 10);
+
+
+  //intlist.printNode();
+  //cout << intlist[2];
+
+  return 0;
 }
-
