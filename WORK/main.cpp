13deb9728fc741ff4adcbbad00b128f774671f77
TR14WR Terriuawara <Terriuawara@gmail.com>
2025-07-27 17:58:17 +0500

    функторы

--- WORK/main.cpp
+++ WORK/main.cpp
@@ -1,30 +1,27 @@
 ﻿#include <iostream>
 #include <thread>
 
-
-
-int Sum(int a, int b) {
-  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-  return a + b;
-}
+struct Functor
+{
+  void operator()(const int a) {
+    if (a % 2 == 0) {
+      SumEvenNum += a;
+      ++CountEvenNum;
+    }
+  }
+  int GetSumEvenNum() { return SumEvenNum; };
+  int GetCountEvenNum() { return CountEvenNum; };
+private:
+  int SumEvenNum = 0, CountEvenNum = 0;
+};
 
 
 int main() {
   setlocale(0, "");
-  int a, b;
-  std::cin >> a >> b;
-  int result;
-
-  std::thread th1([&result, &a, &b]() {result = Sum(a, b); });
-
-
-  for (size_t i = 0; i <= 10; i++)
-  {
-    std::cout << std::this_thread::get_id() << "\tmain " << i << std::endl;
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
-  }
-
-  th1.join();
-  std::cout << "sum result = " << result << std::endl;
+  int arr[]{ 4,12,71,4,17,3,43,32,14,3245,7,243,512,354,665 };
+  Functor f;
+  for (auto el1 : arr)
+    f(el1);
+  std::cout << "Сумма чётных чисел " << f.GetSumEvenNum() << "\tКоличество чётных чисел " << f.GetCountEvenNum() << std::endl;
   return 0;
 }
