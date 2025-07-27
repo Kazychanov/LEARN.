57caafe190cf305df79a3e34c64d1aea85217c30
TR14WR Terriuawara <Terriuawara@gmail.com>
2025-07-27 14:54:20 +0500

    указатели на функцию и functional с функциями

--- WORK/main.cpp
+++ WORK/main.cpp
@@ -1,27 +1,38 @@
 ﻿#include <iostream>
-#include <thread>
+#include <functional>
+#include <vector>
 
+using vectorFunc = std::vector<std::function<void(int)>>;
 
-using namespace std;
-
-void DoWork(int a, int b) {
-  this_thread::sleep_for(chrono::milliseconds(1000));
-  cout << "a+b " << a + b << endl;
+void FOO(int a) {
+  if(a % 2 == 0)
+  std::cout << "FOO " << a << std::endl;
 }
-
+void BAR(int a) {
+  if (a > 50)
+  std::cout << "BAR " << a << std::endl;
+}
+void vfFOO(vectorFunc& vf, std::vector<int>& v) {
+  for (auto a : v) {
+    for (auto f : vf) {
+      f(a);
+    }
+  }
+}
 
 int main() {
   setlocale(0, "");
-  int a, b;
-  cin >> a >> b;
 
-  thread th(DoWork,a,b);
-  for (size_t i = 0; true; i++)
-  {
-    cout << this_thread::get_id() << "\tmain " << i << endl;
-    this_thread::sleep_for(chrono::milliseconds(500));
-  }
-
-  th.join();
+  std::vector<int> v{ 2,4,645,1324,4,54 };
+  vectorFunc vf{ FOO, BAR };
+  vfFOO(vf, v);
+  void(*functptr)(int a);
+  std::cout << "++++++++++++++++++++++++++" << std::endl;
+  functptr = BAR;
+  for(auto a : v)
+    functptr(a);
+  functptr = FOO;
+  for (auto a : v)
+    functptr(a);
   return 0;
 }
