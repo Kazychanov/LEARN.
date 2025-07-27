0ffbdd1f54ffb7e901b1002aeb9480cb7846dbc2
TR14WR Terriuawara <Terriuawara@gmail.com>
2025-07-27 15:45:14 +0500

    возврат результата из другого потока

--- WORK/main.cpp
+++ WORK/main.cpp
@@ -1,12 +1,30 @@
 ﻿#include <iostream>
+#include <thread>
+
+
+
+int Sum(int a, int b) {
+  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  return a + b;
+}
 
 
 int main() {
   setlocale(0, "");
-  int p = 1;
-  int a = 5;
-  auto f = [&p,&a](int a1) {p += a + a1; return true; };
-  f(a);
-  std::cout << p << std::endl;
+  int a, b;
+  std::cin >> a >> b;
+  int result;
+
+  std::thread th1([&result, &a, &b]() {result = Sum(a, b); });
+
+
+  for (size_t i = 0; i <= 10; i++)
+  {
+    std::cout << std::this_thread::get_id() << "\tmain " << i << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  }
+
+  th1.join();
+  std::cout << "sum result = " << result << std::endl;
   return 0;
 }
